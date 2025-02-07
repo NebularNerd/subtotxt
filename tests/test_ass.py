9fc6ff3dde00e472ddabd1c1c9ddae1059442263
Andy <NebularNerd@users.noreply.github.com>
2025-02-07 17:09:59 +0000

    Update test_ass.py

--- tests/test_ass.py
+++ tests/test_ass.py
@@ -8,6 +8,10 @@ import pytest
 
 
 class test_values:
+    """
+    Set file name and options to parse
+    """
+
     def __init__(self):
         self.loca = Path(__file__).parent.absolute()  # Maps basedir to location of this file
         self.file = str(f"{self.loca}/resources/SSA_Example_400plus.ass")
@@ -22,6 +26,9 @@ use_this = test_values()
 
 
 def compare_files():
+    """
+    If both approved output and tested output match, all is good.
+    """
     a = CRC32_from_file(Path(f"{use_this.loca}/resources/outputs/SSA_Example_400plus.txt"))
     b = CRC32_from_file(Path(use_this.outf.name))
     if a != b:
@@ -31,6 +38,9 @@ def compare_files():
 
 
 def CRC32_from_file(filename):
+    """
+    Quick and easy CRC32 checker, nothing fancy.
+    """
     buf = open(filename, "rb").read()
     buf = binascii.crc32(buf) & 0xFFFFFFFF
     return "%08X" % buf
@@ -38,6 +48,9 @@ def CRC32_from_file(filename):
 
 @pytest.fixture
 def in_file():
+    """
+    Set variables to pass through to tester in main script
+    """
     return {
         "test_file": use_this.file,
         "test_force": use_this.utf8,
@@ -49,10 +62,11 @@ def in_file():
 
 
 def test_ass(in_file):
-    # Tests using default options
-    # Temp file should match CRC32 of know good output
-    # Not the most comprehensive test but it's a start.
-
+    """
+    Tests using default options
+    Temp file should match CRC32 of know good output
+    Not the most comprehensive test but it's a start.
+    """
     results = check_it_works(in_file)
     assert results is None
 
